1fa2084bf841ad0a0a3043e33581350e26008945
Tim Echtenkamp <techtenkamp@noblhealth.com>
2023-12-17 08:17:48 -0600

    day 7 part 2

--- rust/src/day7/mod.rs
+++ rust/src/day7/mod.rs
@@ -21,16 +21,21 @@ pub fn run_part1() {
             hands.push(Hand::new(cards, bid));
         }
     }
+    let winnings = calculate_winnings(hands);
+    println!("Total winnings: {}", winnings);
+}
+
+fn calculate_winnings(mut hands: Vec<Hand>) -> i32 {
     hands.sort();
 
     let mut winnings = 0;
     for (rank, hand) in hands.iter().enumerate() {
         winnings += (rank+1) as i32 * hand.bid;
     }
-    println!("Total winnings: {}", winnings);
+    winnings
 }
 
-#[derive(Eq, PartialEq, Debug)]
+#[derive(Eq, PartialEq, Debug, Clone, Copy)]
 struct Hand {
     cards: [Card; 5],
     bid: i32
@@ -51,7 +56,7 @@ impl From<char> for Card {
 impl Ord for Card {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         let val = |x: char| {
-            match x { 'A' => 14 as u8, 'K' => 13, 'Q' => 12, 'J' => 11, 'T' => 10, x => x.to_digit(10).unwrap() as u8 }
+            match x { 'A' => 14 as u8, 'K' => 13, 'Q' => 12, 'J' => 11, 'T' => 10, 'j' => 1, x => x.to_digit(10).unwrap() as u8 }
         };
         val(self.value).cmp(&val(other.value))
     }
@@ -76,6 +81,18 @@ impl Hand {
             let mut card_map = HashMap::new();
             for card in self.cards.iter() {
                 card_map.entry(card).and_modify(|e| *e += 1).or_insert(1 as u8);
+            }
+            // jokers are wild, so before evaluating the general strength, add the 'j' to the highest other block
+            let jokers = card_map.remove(&Card { value: 'j' });
+            if let Some(j) = jokers {
+                let max_val = card_map.values_mut().max();
+                match max_val {
+                    Some(max_val) => {*max_val += j;},
+                    None => {
+                        // a whole hand of jokers!
+                        return HandStrength::FIVEOFAKIND;
+                    }
+                }
             }
             match card_map.values().max().unwrap() {
                 5 => HandStrength::FIVEOFAKIND,
@@ -123,5 +140,26 @@ enum HandStrength {
 }
 
 pub fn run_part2() {
+    let mut buf = Vec::new();
+    let lines = get_input_lines(&get_file_path!("input.txt"), & mut buf);
 
+    let mut hands = Vec::new();
+    for l in lines {
+        if let Ok(line) = l.as_ref() {
+            let mut hand = scan! {line;
+                (let hand: NonSpace<String>, let bid: i32) => Hand::new(hand, bid)
+            };
+            match hand.as_mut() {
+                Ok(hand) => { 
+                    hand.cards.iter_mut().for_each(|card| { if card.value == 'J' { card.value = 'j' }});
+                    hands.push(hand.to_owned());
+                },
+                Err(scan_error) => { println!("Scan Error: {:?}", scan_error); }
+            }
+        }
+    }
+
+    let winnings = calculate_winnings(hands);
+
+    println!("Total winnings: {}", winnings);
 }
